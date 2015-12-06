e6a500d20188f264252afc84da365f81c7ea18f2
milovanderlinden <milo@dogodigi.net>
2015-12-06 15:03:16 +0100

    Using NODE_ENV=test for test

--- server.js
+++ server.js
@@ -53,21 +53,29 @@ if ('production' == env) {
 }
 
 function start() {
-  require('./models').sequelize.sync({
-      force: true,
-      logging: console.log
-    })
-    .then(function(err) {
-      console.log("Sync succesful");
-      app.listen(app.get('port'), function() {
-        console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+  if ('test' == env || 'development' == env) {
+    console.log('Running in ' + env + ' mode');
+    require('./models').sequelize.sync({
+        force: true,
+        logging: console.log
+      })
+      .then(function(err) {
+        console.log("Sync succesful");
+        app.listen(app.get('port'), function() {
+          console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+        });
+      })
+      .then(function(err) {
+        if (err) {
+          console.log("Sync failed:", err);
+        }
       });
-    })
-    .then(function(err) {
-      if (err) {
-        console.log("Sync failed:", err);
-      }
+  } else if ('production' == env) {
+    console.log('Running in production mode');
+    app.listen(app.get('port'), function() {
+      console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
     });
+  }
 }
 exports.start = start;
 exports.app = app;
--- test/testGeoreportv2.js
+++ test/testGeoreportv2.js
@@ -1,50 +1,65 @@
 var request = require('supertest'),
   server = require('../server.js'),
   assert = require('assert');
-  var Open311 = require('open311');
-
-  describe('Open311()', function() {
-    describe('new Open311()', function() {
-      var open311;
-
-      before(function(done) {
-        open311 = new Open311({
-          endpoint: 'http://localhost:3000/api/',
-          format: 'xml',
-          jurisdiction: 'eindhoven.nl'
-        });
-        done();
-      });
-
-      it("should attach options to `this`", function(done) {
-        assert.equal('http://localhost:3000/api/',open311.endpoint);
-        assert.equal('xml',open311.format);
-        assert.equal('eindhoven.nl',open311.jurisdiction);
-        done();
-      });
-  });
-});
 
 server.app.set('env', 'test');
+var Open311 = require('open311');
 
-describe('Discovery', function() {
-  it('GET /', function(done) {
-    // See that we get a status 200 on retrieving the Index
-    request(server.app).get('/')
-      .expect(404, done);
-  });
+describe('Open311-api', function() {
+  describe('Georeport v2 testSuite', function() {
+    var open311;
 
-  it('GET /api/services.xml', function(done) {
-    // See that we get a status 200 on retrieving the Index
-    request(server.app).get('/api/services.xml')
-      .expect('Content-Type', 'text/xml; charset=utf-8')
-      .expect(200, done);
-  });
+    before(function(done) {
+      require('../models').sequelize.sync({
+          force: true,
+          logging: null
+        })
+        .then(function(err) {
+          open311 = new Open311({
+            endpoint: 'http://localhost:3000/api/',
+            format: 'xml',
+            jurisdiction: 'eindhoven.nl'
+          });
+          done();
+        });
+    });
 
-  it('GET /api/discovery.xml', function(done) {
-    // See that we get a status 200 on retrieving the Index
-    request(server.app).get('/api/discovery.xml')
-      .expect('Content-Type', 'text/xml; charset=utf-8')
-      .expect(200, done);
+    it("open311 client should be initialized", function(done) {
+      assert.equal('http://localhost:3000/api/', open311.endpoint);
+      assert.equal('xml', open311.format);
+      assert.equal('eindhoven.nl', open311.jurisdiction);
+      done();
+    });
+    it('GET /', function(done) {
+      // See that we get a status 200 on retrieving the Index
+      request(server.app).get('/')
+        .expect(404, done);
+    });
+
+    it('GET /api/services.json', function(done) {
+      // See that we get a status 200 on retrieving the Index
+      request(server.app).get('/api/services.json')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, done);
+    });
+
+    it('GET /api/services.xml', function(done) {
+      // See that we get a status 200 on retrieving the Index
+      request(server.app).get('/api/services.xml')
+        .expect('Content-Type', 'text/xml; charset=utf-8')
+        .expect(200, done);
+    });
+    it('GET /api/discovery.json', function(done) {
+      // See that we get a status 200 on retrieving the Index
+      request(server.app).get('/api/discovery.json')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, done);
+    });
+    it('GET /api/discovery.xml', function(done) {
+      // See that we get a status 200 on retrieving the Index
+      request(server.app).get('/api/discovery.xml')
+        .expect('Content-Type', 'text/xml; charset=utf-8')
+        .expect(200, done);
+    });
   });
 });
