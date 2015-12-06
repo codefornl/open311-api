cba05298577e80516230eddc57b3620c9d0e2701
milovanderlinden <milo@dogodigi.net>
2015-12-06 13:18:52 +0100

    Added open311 to test

--- test/testGeoreportv2.js
+++ test/testGeoreportv2.js
@@ -1,6 +1,29 @@
 var request = require('supertest'),
   server = require('../server.js'),
   assert = require('assert');
+  var Open311 = require('open311');
+
+  describe('Open311()', function() {
+    describe('new Open311()', function() {
+      var open311;
+
+      before(function(done) {
+        open311 = new Open311({
+          endpoint: 'http://localhost:3000/api/',
+          format: 'xml',
+          jurisdiction: 'eindhoven.nl'
+        });
+        done();
+      });
+
+      it("should attach options to `this`", function(done) {
+        assert.equal('http://localhost:3000/api/',open311.endpoint);
+        assert.equal('xml',open311.format);
+        assert.equal('eindhoven.nl',open311.jurisdiction);
+        done();
+      });
+  });
+});
 
 server.app.set('env', 'test');
 
@@ -10,7 +33,7 @@ describe('Discovery', function() {
     request(server.app).get('/')
       .expect(404, done);
   });
-  
+
   it('GET /api/services.xml', function(done) {
     // See that we get a status 200 on retrieving the Index
     request(server.app).get('/api/services.xml')
